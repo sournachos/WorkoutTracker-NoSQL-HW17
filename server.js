33672cadcdc0c476819f69983b31242570de6366
sournachos <joserivera18100@gmail.com>
2021-05-14 00:03:30 -0500

    Will finish later

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(compression());
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
